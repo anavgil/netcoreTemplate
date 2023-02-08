54c9b841ae230e32a569c576d6dd3555be79534c
Alberto Navarro Gil <albertonavarrogil@gmail.com>
2023-02-08 09:12:59 +0100

    Added reposity abstractions

--- netcoreTemplate.Domain/Interfaces/IQueryRepository.cs
+++ netcoreTemplate.Domain/Interfaces/IQueryRepository.cs
@@ -8,5 +8,7 @@ namespace netcoreTemplate.Domain.Interfaces
 {
     public interface IQueryRepository<T> where T : class
     {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Int64 id);
     }
 }
