d11049e5b770d28a49f624716c611faa95c629a5
Alberto Navarro Gil <albertonavarrogil@gmail.com>
2023-02-10 10:52:29 +0100

    Added IHttpClientFacto to Infra

---
+++ netcoreTemplate.Infrastructure/Agents/TestAgent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netcoreTemplate.Infrastructure.Agents
+{
+    public class TestAgent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestAgent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private async Task TestHttpClient()
+        {
+            using HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            await httpClient.GetAsync("");
+
+
+            await Task.CompletedTask;
+        }
+    }
+}
