3f0bb7cb6063e325519e979e1f66b503b6fcd557
Alberto Navarro Gil <a.navarro@innovation.es>
2023-07-11 17:05:46 +0200

    Update nugets and added IdentityCore

--- netcoreTemplate.Infrastructure/DependencyInjectionExtension.cs
+++ netcoreTemplate.Infrastructure/DependencyInjectionExtension.cs
@@ -1,17 +1,31 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using netcoreTemplate.Domain.Interfaces;
+using netcoreTemplate.Infrastructure.Persistence.Identity;
+using netcoreTemplate.Infrastructure.Persistence.Identity.Model;
 using netcoreTemplate.Infrastructure.Repositories.Base;
+using System.Reflection;
 
 namespace netcoreTemplate.Infrastructure
 {
     public static class DependencyInjectionExtension
     {
 
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
         {
+            services.AddSqlite<IdentityContext>(configuration.GetConnectionString("Default"), options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            })
+            .AddIdentityCore<User>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<IdentityContext>();
 
             services.AddHttpClient();
             services.AddScoped<IUnitOfWork,UnitOfWork>();
+
             return services;
         }
     }
