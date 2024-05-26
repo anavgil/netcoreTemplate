e24fc9b759ee714c81a6665835bf096734818568
Alberto Navarro Gil <albertonavarrogil@gmail.com>
2024-05-26 18:22:15 +0200

    Refactor code

--- netcoreTemplate.Application/Features/Feature1/Validators/TestQueryParamRequestValidator.cs
+++ netcoreTemplate.Application/Features/Feature1/Validators/TestQueryParamRequestValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
+using netcoreTemplate.Application.Features.Feature1.CQRS.Querys;
 
-namespace netcoreTemplate.Application.Features.Feature1.CQRS.Querys;
+namespace netcoreTemplate.Application.Features.Feature1.Validators;
 
 public class TestQueryParamRequestValidator : AbstractValidator<TestQueryParamRequestRequest>
 {
