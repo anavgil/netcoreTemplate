2fa7c2431291f6d0848d977697dc305437a120a2
Alberto Navarro Gil <a.navarro@innovation.es>
2024-10-21 15:06:02 +0200

    Added CustomExceptionHandler

---
+++ netcoreTemplate.Api/Middlewares/CustomExceptionHandler.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Middlewares;
+
+public class CustomExceptionHandler(IProblemDetailsService problemDetailsService, ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
+{
+    private const string validationExceptionTitle = "One or more validation errors occurred.";
+    private const string validationExceptionType = "https://tools.ietf.org/html/rfc7231#section-6.5.1";
+    private const string standarExceptionTitle = "One error occurred.";
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var exceptionMessage = exception.Message;
+        var problemDetails = new ProblemDetails()
+        {
+            Status = GetStatuscodeFromException(exception),
+            Detail = exception.Message,
+        };
+
+        _logger.LogError(exception,
+            "Error Message: {}, Time  of occurrence {time}", exceptionMessage, DateTime.UtcNow);
+
+        if (exception is ValidationException fluentException)
+        {
+            problemDetails.Title = validationExceptionTitle;
+            problemDetails.Type = validationExceptionType;
+            List<string> validationErrors = [];
+            foreach (var error in fluentException.Errors)
+            {
+                validationErrors.Add(error.ErrorMessage);
+            }
+            problemDetails.Extensions.Add("errors", validationErrors);
+        }
+        else
+        {
+            problemDetails.Title = standarExceptionTitle;
+            problemDetails.Type = exception.GetType().Name;
+
+            if (exception.InnerException is not null)
+            {
+                problemDetails.Extensions = new Dictionary<string, object>()
+            {
+                { "INNER-Message",exception.InnerException.Message },
+                { "INNER-Type",exception.InnerException.GetType().Name}
+            };
+            }
+        }
+
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            Exception = exception,
+            HttpContext = httpContext,
+            ProblemDetails = problemDetails
+        });
+
+    }
+
+    private static int GetStatuscodeFromException(Exception exception) => exception switch
+    {
+        BadHttpRequestException => StatusCodes.Status400BadRequest,
+        ValidationException => StatusCodes.Status400BadRequest,
+        _ => StatusCodes.Status500InternalServerError
+    };
+}
--- netcoreTemplate.api/Program.cs
+++ netcoreTemplate.api/Program.cs
@@ -2,6 +2,8 @@ using Api.Endpoints;
 using Api.Middlewares;
 using Application;
 using Infrastructure;
+using Microsoft.AspNetCore.Http.Features;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +14,20 @@ builder.AddServiceDefaults();
 //builder.Services.AddControllers();
 //builder.Services.RegisterJwtAuthentication(builder.Configuration);
 builder.Services
-        .AddExceptionHandler<GlobalExceptionHandler>()
-        .AddProblemDetails()
+        .AddExceptionHandler<CustomExceptionHandler>()
+        .AddProblemDetails(options => 
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Instance =
+                    $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+
+                Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+                context.ProblemDetails.Extensions.TryAdd("traceId", activity?.Id);
+
+            }
+            )
         .AddEndpoints(typeof(Program).Assembly);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
